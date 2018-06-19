5f647f59f075889abb8f8e83c759269751fbbbd9
fangzichen <zichenfang@yeah.net>
2018-06-19 10:07:51 +0800

    Initial Commit

---
+++ CommunityHospital/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommunityHospital
+//
+//  Created by 方子辰 on 2018/6/19.
+//  Copyright © 2018年 方子辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommunityHospital/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommunityHospital
+//
+//  Created by 方子辰 on 2018/6/19.
+//  Copyright © 2018年 方子辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
